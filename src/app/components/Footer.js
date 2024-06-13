d971e208210ae0cf88d3d28c77c3add5e3c4d590
Ese Oxalde <ese.oxalde@gmail.com>
2024-06-13 14:03:25 -0300

    se agregaron sections y title a los links

--- src/app/components/Footer.js
+++ src/app/components/Footer.js
@@ -3,10 +3,10 @@ import styles from './Footer.module.css';
 
 export default function Footer() {
   return (
-    <footer className={styles.footer}>
+    <footer className={styles.footer} role="contentinfo">
       <div className={styles.container}>
-        <div className={styles.leftSection}>
-          <img src="/logo.png" alt="Logo" className={styles.logo} />
+        <section className={styles.leftSection}>
+          <img src="/logo.png" alt="Logo de Memorias de Malvinas" className={styles.logo} />
           <div className={styles.socialMedia}>
             <a href="https://facebook.com" target="_blank" rel="noopener noreferrer">
               <img src="/facebook.svg" alt="Facebook" className={styles.socialIcon} />
@@ -18,39 +18,39 @@ export default function Footer() {
               <img src="/instagram.svg" alt="Instagram" className={styles.socialIcon} />
             </a>
           </div>
-        </div>
-        <div className={styles.rightSection}>
+        </section>
+        <section className={styles.rightSection}>
           <div className={styles.column}>
             <h3>Nosotros</h3>
             <ul>
-              <li><Link href="/link1">Acerca de Memorias de Malvinas</Link></li>
-              <li><Link href="/link2">Blog</Link></li>
+              <li><Link href="/acerca" title="Acerca de Memorias de Malvinas">Acerca de Memorias de Malvinas</Link></li>
+              <li><Link href="/blog" title="Blog">Blog</Link></li>
             </ul>
           </div>
           <div className={styles.column}>
             <h3>Alianzas</h3>
             <ul>
-              <li><Link href="/link1">Editoriales</Link></li>
-              <li><Link href="/link2">Universidades</Link></li>
-              <li><Link href="/link3">Contribuidores</Link></li>
+              <li><Link href="/editoriales" title="Editoriales">Editoriales</Link></li>
+              <li><Link href="/universidades" title="Universidades">Universidades</Link></li>
+              <li><Link href="/contribuidores" title="Contribuidores">Contribuidores</Link></li>
             </ul>
           </div>
           <div className={styles.column}>
             <h3>Soporte</h3>
             <ul>
-              <li><Link href="/link1">Ayuda</Link></li>
-              <li><Link href="/link2">Contctános</Link></li>
+              <li><Link href="/ayuda" title="Ayuda">Ayuda</Link></li>
+              <li><Link href="/contacto" title="Contacto">Contctános</Link></li>
             </ul>
           </div>
           <div className={styles.column}>
             <h3>Legal</h3>
             <ul>
-              <li><Link href="/link1">Terminos</Link></li>
-              <li><Link href="/link2">Privacidad</Link></li>
-              <li><Link href="/link3">Copyright</Link></li>
+              <li><Link href="/terminos" title="Terminos">Terminos</Link></li>
+              <li><Link href="/privacidad" title="Privacidad">Privacidad</Link></li>
+              <li><Link href="/copyright" title="Copyright">Copyright</Link></li>
             </ul>
           </div>
-        </div>
+        </section>
       </div>
     </footer>
   );
