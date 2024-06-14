4c43b40e5d8d39712b12f56e80d55209b49467a8
Ese Oxalde <ese.oxalde@gmail.com>
2024-06-14 15:47:34 -0300

    Cambios en footer, se agregaron iconos, se acomodó el logo y cambios de estilo

--- src/app/components/Footer.js
+++ src/app/components/Footer.js
@@ -1,37 +1,50 @@
 import Link from 'next/link';
 import styles from './Footer.module.css';
+import Image from 'next/image';
 
 export default function Footer() {
   return (
     <footer className={styles.footer} role="contentinfo">
       <div className={styles.container}>
         <section className={styles.leftSection}>
-          <img src="/logo.png" alt="Logo de Memorias de Malvinas" className={styles.logo} />
+
+        <div className={styles.logo}>
+          <Link href="/" legacyBehavior>
+            <a className={styles.logoLink} title="Inicio">
+              <div className={styles.textContainer}>
+                <span className={styles.logoTextTop}>Memorias de</span>
+                <span className={styles.logoTextBottom}>Malvinas</span>
+              </div>
+              <Image src="/casco.svg" alt="" width={50} height={50} className={styles.logoImage} />
+            </a>
+          </Link>
+        </div>
+          
           <div className={styles.socialMedia}>
             <a href="https://facebook.com" target="_blank" rel="noopener noreferrer">
-              <img src="/facebook.svg" alt="Facebook" className={styles.socialIcon} />
+              <Image src="/redes/facebook.svg" alt="Facebook" className={styles.socialIcon} width={30} height={30} />
             </a>
             <a href="https://twitter.com" target="_blank" rel="noopener noreferrer">
-              <img src="/twitter.svg" alt="Twitter" className={styles.socialIcon} />
+              <Image src="/redes/twitter.svg" alt="Twitter" className={styles.socialIcon} width={30} height={30}/>
             </a>
             <a href="https://instagram.com" target="_blank" rel="noopener noreferrer">
-              <img src="/instagram.svg" alt="Instagram" className={styles.socialIcon} />
+              <Image src="/redes/instagram.svg" alt="Instagram" className={styles.socialIcon}width={30} height={30} />
             </a>
             <a href="https://youtube.com" target="_blank" rel="noopener noreferrer">
-              <img src="/youtube.svg" alt="Yourube" className={styles.socialIcon} />
+              <Image src="/redes/youtube.svg" alt="Yourube" className={styles.socialIcon} width={30} height={30}/>
             </a>
           </div>
         </section>
         <section className={styles.rightSection}>
           <div className={styles.column}>
-            <h3>Nosotros</h3>
+            <p>Nosotros</p>
             <ul>
               <li><Link href="/nosotros" title="Acerca de Memorias de Malvinas">Acerca de Memorias de Malvinas</Link></li>
               <li><Link href="/blog" title="Blog">Blog</Link></li>
             </ul>
           </div>
           <div className={styles.column}>
-            <h3>Alianzas</h3>
+            <p>Alianzas</p>
             <ul>
               <li><Link href="/editoriales" title="Editoriales">Editoriales</Link></li>
               <li><Link href="/universidades" title="Universidades">Universidades</Link></li>
@@ -39,14 +52,14 @@ export default function Footer() {
             </ul>
           </div>
           <div className={styles.column}>
-            <h3>Soporte</h3>
+            <p>Soporte</p>
             <ul>
               <li><Link href="/ayuda" title="Ayuda">Ayuda</Link></li>
               <li><Link href="/contacto" title="Contacto">Contctános</Link></li>
             </ul>
           </div>
           <div className={styles.column}>
-            <h3>Legal</h3>
+            <p>Legal</p>
             <ul>
               <li><Link href="/terminos" title="Terminos">Terminos</Link></li>
               <li><Link href="/privacidad" title="Privacidad">Privacidad</Link></li>
