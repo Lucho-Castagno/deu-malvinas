201cb30c145ac8e006c024157f57d8263cfbe95e
Ese Oxalde <ese.oxalde@gmail.com>
2024-06-26 03:12:23 -0300

    Cambio en los links del footer

--- src/app/components/Footer.js
+++ src/app/components/Footer.js
@@ -9,17 +9,17 @@ export default function Footer() {
         <div className="row">
           <div className="col-md-4 d-flex flex-column align-items-center align-items-md-start mb-4 mb-md-0">
             <Link href="/" legacyBehavior>
-              <a className="d-flex align-items-center mb-3 navbar-brand" title="Inicio">
+              <a className="d-flex align-items-center mb-3 navbar-brand">
                 <div className="text-start">
                   <span className="d-block">Memorias de</span>
                   <span className="d-block">Malvinas</span>
                 </div>
-                <Image src="/casco.svg" alt="Logo" width={50} height={50} className="ml-2" />
+                <Image src="/casco.svg" alt="Logo de Memorias de Malvinas" width={50} height={50} className="ml-2" />
               </a>
             </Link>
-            <div className="d-flex justify-content-center">
+            <div className={`d-flex justify-content-center`}>
               <a href="https://facebook.com" target="_blank" rel="noopener noreferrer" className="mx-2">
-                <Image src="/redes/facebook.svg" alt="Facebook" width={30} height={30} />
+                <Image src="/redes/facebook.svg" alt="Facebook" width={30} height={30}/>
               </a>
               <a href="https://twitter.com" target="_blank" rel="noopener noreferrer" className="mx-2">
                 <Image src="/redes/twitter.svg" alt="Twitter" width={30} height={30} />
@@ -33,33 +33,27 @@ export default function Footer() {
             </div>
           </div>
           <div className="col-md-2 mb-4 mb-md-0">
-            <h5>Nosotros</h5>
+            <p className={`${styles.customP}`}>Enlaces rápidos</p>
             <ul className="list-unstyled">
-              <li><Link href="/nosotros" legacyBehavior><a className="text-white">Acerca de Memorias de Malvinas</a></Link></li>
-              <li><Link href="/blog" legacyBehavior><a className="text-white">Blog</a></Link></li>
+              <li><Link href="/" legacyBehavior><a className={`${styles.customLink}`}>Inicio</a></Link></li>
+              <li><Link href="/veteranos" legacyBehavior><a className={`${styles.customLink}`}>Veteranos</a></Link></li>
+              <li><Link href="/mapa" legacyBehavior><a className={`${styles.customLink}`}>Mapa</a></Link></li>
             </ul>
           </div>
           <div className="col-md-2 mb-4 mb-md-0">
-            <h5>Alianzas</h5>
+            <p className={`${styles.customP}`}>Soporte</p>
             <ul className="list-unstyled">
-              <li><Link href="/editoriales" legacyBehavior><a className="text-white">Editoriales</a></Link></li>
-              <li><Link href="/universidades" legacyBehavior><a className="text-white">Universidades</a></Link></li>
-              <li><Link href="/contribuidores" legacyBehavior><a className="text-white">Contribuidores</a></Link></li>
+              <li><Link href="/ayuda" legacyBehavior><a className={`${styles.customLink}`}>Ayuda</a></Link></li>
+              <li><Link href="/personalizacion" legacyBehavior><a className={`${styles.customLink}`}>Personalizacion</a></Link></li>
+              <li><Link href="/contacto" legacyBehavior><a className={`${styles.customLink}`}>Contáctanos</a></Link></li>
             </ul>
           </div>
           <div className="col-md-2 mb-4 mb-md-0">
-            <h5>Soporte</h5>
+            <p className={`${styles.customP}`}>Legal</p>
             <ul className="list-unstyled">
-              <li><Link href="/ayuda" legacyBehavior><a className="text-white">Ayuda</a></Link></li>
-              <li><Link href="/contacto" legacyBehavior><a className="text-white">Contáctanos</a></Link></li>
-            </ul>
-          </div>
-          <div className="col-md-2 mb-4 mb-md-0">
-            <h5>Legal</h5>
-            <ul className="list-unstyled">
-              <li><Link href="/terminos" legacyBehavior><a className="text-white">Términos</a></Link></li>
-              <li><Link href="/privacidad" legacyBehavior><a className="text-white">Privacidad</a></Link></li>
-              <li><Link href="/copyright" legacyBehavior><a className="text-white">Copyright</a></Link></li>
+              <li><Link href="/terminos" legacyBehavior><a className={`${styles.customLink}`}>Términos</a></Link></li>
+              <li><Link href="/privacidad" legacyBehavior><a className={`${styles.customLink}`}>Privacidad</a></Link></li>
+              <li><Link href="/copyright" legacyBehavior><a className={`${styles.customLink}`}>Copyright</a></Link></li>
             </ul>
           </div>
         </div>
