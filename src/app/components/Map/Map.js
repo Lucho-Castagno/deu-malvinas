328f8540b155e167a706dd89b4490723cab99c5d
lcastagno <lucho27ris@gmail.com>
2024-06-28 19:10:52 -0300

    fix. mapa

---
+++ src/app/components/Map/Map.js
@@ -0,0 +1,22 @@
+import { MapContainer, Marker, TileLayer, Tooltip, Popup } from "react-leaflet"
+import "leaflet/dist/leaflet.css"
+import "leaflet-defaulticon-compatibility"
+import "leaflet-defaulticon-compatibility/dist/leaflet-defaulticon-compatibility.css"
+
+export default function Map() {
+    return (
+        <div className="titulo">
+            <MapContainer center={[51.505, -0.09]} zoom={13} scrollWheelZoom={false} className="vw-100 vh-100">
+                <TileLayer
+                attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+                url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                />
+                <Marker position={[51.505, -0.09]}>
+                <Popup>
+                    A pretty CSS3 popup. <br /> Easily customizable.
+                </Popup>
+                </Marker>
+            </MapContainer>
+        </div>
+    )
+}
--- src/app/mapa/page.js
+++ src/app/mapa/page.js
@@ -1,35 +1,22 @@
 'use client';
+import { useMemo } from "react";
 import styles from "./mapa.modules.css";
-
-import {
-  MapContainer,
-  TileLayer,
-  useMap,
-  Marker,
-  Popup
-} from 'react-leaflet';
+import dynamic from "next/dynamic";
 
 export default function MapaPage() {
-    return (
-      <main role="main" className="main container my-5 py-5">
-      <section>
-        <div className="titulo mb-5">
-          <h1 className="mb-4">Mapa de veteranos de guerra de Malvinas</h1>
+  const Map = useMemo(() => dynamic(
+    () => import('../components/Map'),
+    { 
+      loading: () => <p>A map is loading</p>,
+      ssr: false
+    }
+  ), []);
 
-          <MapContainer center={[51.505, -0.09]} zoom={13} scrollWheelZoom={false}>
-          <TileLayer
-             attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-              url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-          />
-          <Marker position={[51.505, -0.09]}>
-            <Popup>
-              A pretty CSS3 popup. <br /> Easily customizable.
-            </Popup>
-          </Marker>
-        </MapContainer>
-        </div>
+  return (
+    <main role="main" className="main">
+      <section>
+        <Map/>
       </section>
-    
     </main>
   );
-  }
+}
