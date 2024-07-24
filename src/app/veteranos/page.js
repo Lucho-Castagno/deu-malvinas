f55d8c9c02ab57ed4c8d625451a86502e09cdb9f
lcastagno <lucho27ris@gmail.com>
2024-07-24 17:45:38 -0300

    Buscador de Heroes

--- src/app/veteranos/page.js
+++ src/app/veteranos/page.js
@@ -1,59 +1,99 @@
 "use client";
 
-import React from 'react';
-import Image from 'next/image';
-import { useGeoJsonData } from '../hook/useGeoJsonData';
-import { useRouter } from 'next/navigation';
+import styles from './Veteranos.module.css';
+import React, { useState, useEffect } from "react";
+import Image from "next/image";
+import { useGeoJsonData } from "../hook/useGeoJsonData";
+import { useRouter } from "next/navigation";
 
 function processUrl(url) {
-  if (url.startsWith('.')) {
+  if (url.startsWith(".")) {
     url = url.substring(1);
   }
 
-  const validExtensions = ['.png', '.jpg', '.jpeg', '.gif'];
-  const extIndex = url.lastIndexOf('.');
+  const validExtensions = [".png", ".jpg", ".jpeg", ".gif"];
+  const extIndex = url.lastIndexOf(".");
   if (extIndex !== -1) {
     const ext = url.substring(extIndex);
     if (!validExtensions.includes(ext)) {
-      url = url.substring(0, extIndex) + '.png';
+      url = url.substring(0, extIndex) + ".png";
     }
   } else {
-    url += '.png';
+    url += ".png";
   }
 
   return url;
 }
 
-export default function HistoriasPage() {
+export default function VeteranosPage() {
   const veterans = useGeoJsonData();
-  const baseUrl = 'https://www.bienestar.mil.ar/malvinas';
-  const defaultPhotoUrl = 'soldado2.png'; // Ruta de la imagen por defecto
+  const [searchTerm, setSearchTerm] = useState("");
+  const [filteredVeterans, setFilteredVeterans] = useState([]);
+  const baseUrl = "https://www.bienestar.mil.ar/malvinas";
+  const defaultPhotoUrl = "soldado2.png"; // Ruta de la imagen por defecto
 
   const router = useRouter();
 
+  useEffect(() => {
+    setFilteredVeterans(veterans);
+  }, [veterans]);
+
+  useEffect(() => {
+    if (searchTerm === "") {
+      setFilteredVeterans(veterans);
+    } else {
+      const results = veterans.filter((veteran) => {
+        const props = veteran.properties;
+        return (
+          props && props.Nombre.toLowerCase().includes(searchTerm.toLowerCase())
+        );
+      });
+      setFilteredVeterans(results);
+    }
+  }, [searchTerm, veterans]);
+
   const handleMoreInfo = (veteran) => {
-    localStorage.setItem('veteran', JSON.stringify(veteran));
+    localStorage.setItem("veteran", JSON.stringify(veteran));
     router.push("/historiaveterano");
   };
 
   return (
     <main className="container my-5 p-5" role="main">
-      <h1 className="text-center titulo mb-5">Historias de valor en las Malvinas</h1>
+      <h1 className="text-center titulo mb-5">
+        Historias de valor en las Malvinas
+      </h1>
+      <input
+          type="text"
+          placeholder="Buscar por nombre"
+          aria-label="Escribe el nombre del veterano aquí para facilitar su búsqueda"
+          className={`form-control mb-4 ${styles.searchInput}`}
+          value={searchTerm}
+          onChange={(e) => setSearchTerm(e.target.value)}
+        />
       <div className="row">
-        {veterans.map((veteran, index) => {
+        {filteredVeterans.map((veteran, index) => {
           const props = veteran.properties;
-          const photoUrl = props && props.Foto ? `${baseUrl}${processUrl(props.Foto)}` : defaultPhotoUrl;
+          const photoUrl =
+            props && props.Foto
+              ? `${baseUrl}${processUrl(props.Foto)}`
+              : defaultPhotoUrl;
 
           return (
             <div className="col-md-6 col-lg-3 mb-4" key={index}>
               <div className="card h-100 bg-lightyellow p-3">
                 {props && (
                   <>
-                  <div className='w-100 d-flex justify-content-center align-items-center'>
-                    <div style={{ width: '128px', height: '128px' }}>
-                      <Image src={photoUrl} className="rounded-circle" alt={props.Nombre} width={128} height={128} />
+                    <div className="w-100 d-flex justify-content-center align-items-center">
+                      <div style={{ width: "128px", height: "128px" }}>
+                        <Image
+                          src={photoUrl}
+                          className="rounded-circle"
+                          alt={props.Nombre}
+                          width={128}
+                          height={128}
+                        />
+                      </div>
                     </div>
-                  </div>
                     <div className="card-body">
                       <h5 className="card-title">{props.Nombre}</h5>
                       <p className="card-text">{props.Escalafon}</p>
