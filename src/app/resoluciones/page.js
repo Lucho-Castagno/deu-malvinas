1abd346a9f8f837a9c9706daa15c560d130f9f71
Ese Oxalde <ese.oxalde@gmail.com>
2024-07-30 05:03:27 -0300

    Algo sobre las resoluciones de la ONU, falta acomodar bien el texto

--- src/app/resoluciones/page.js
+++ src/app/resoluciones/page.js
@@ -1,12 +1,59 @@
 import "../globals.css";
+import styles from './Resoluciones.module.css';
+
 
 export default function ResolucionesPage() {
   return (
     <div className="container my-5 py-5" role="main">
-      <h1 className={`text-center subtitle mb-5`}>Últimas resoluciones</h1>
+      <h1 className={`text-center subtitle mb-5`}>Resoluciones de la ONU</h1>
       <div className="row"> 
         <div className="col-md-12">
-         
+        <p className={styles.titulo}>1960</p>
+          <p>El 14 de diciembre de 1960 la Asamblea General de las Naciones Unidas adoptó la resolución 1514 (XV) “Declaración sobre la concesión de la independencia a los países y pueblos coloniales”.</p>
+          <p>Ella marcó un hito en el proceso de descolonización impulsado por las Naciones Unidas, proclamando “la necesidad de poner fin, rápida e incondicionalmente, al colonialismo en todas sus formas y manifestaciones”,
+           y consagrando los dos principios fundamentales que debían guiar el proceso de descolonización: el de libre determinación y el de integridad territorial.</p>
+        <p className={styles.titulo}>1964</p>
+          <p>En septiembre de 1964, durante el debate del Subcomité III del Comité Especial de Descolonización, el Delegado argentino, el Embajador José María Ruda, 
+          sostuvo que no existe en las islas una población sojuzgada, subyugada o sometida al colonialismo, siendo ésta una de las razones por las cuales no corresponde 
+          a dicha población el derecho a la libre determinación de los pueblos que alega la Parte británica.</p>
+          <p>Reclamó, en consecuencia, la aplicación del principio de integridad territorial, reconocido en el párrafo dispositivo sexto de la resolución 1514 (XV) de la Asamblea General, la cual había sido quebrantada con 
+            la usurpación y ocupación británica de esa parte del territorio nacional argentino.</p>
+        <p className={styles.titulo}>1965</p>
+          <p>el 16 de diciembre de 1965 la Asamblea General adoptó la resolución 2065 (XX), la primera referida exclusivamente a la Cuestión Malvinas, a través de la cual reconoció la existencia de 
+            una disputa de soberanía entre la Argentina y el Reino Unido e invitó a ambos países a negociar para encontrar una solución pacífica a la controversia.</p>
+        <p className={styles.titulo}>1966-1982</p>
+          <p>La primera rueda de negociaciones tuvo lugar en julio de 1966 en Londres, una segunda rueda se desarrolló entre noviembre y diciembre del mismo año, también en Londres</p>
+          <p>En el curso de 1967 se pasó a una etapa de conversaciones informales permanentes, que permitían mantener el ritmo negociador sin limitarse a las ruedas formales</p>
+          <p>...</p>
+        <p className={styles.titulo}>1973</p>
+          <p>La resolución 3160 (XXVIII) de la Asamblea General reconocía "los continuos esfuerzos realizados por el gobierno argentino" y declaraba "la necesidad de que se aceleren las 
+          negociaciones previstas en la Resolución 2065 (XX) [...] para arribar a una solución pacífica de la disputa de soberanía existente" entre ambos gobiernos sobre las Islas Malvinas.
+           A partir de la firme actitud argentina y la visión ampliamente mayoritaria de la comunidad internacional, ambos gobiernos ensayaron en los años sucesivos algunas alternativas para reencauzar la negociación.
+           Así ocurrió con la propuesta británica de junio de 1974 tendiente a establecer un condominio anglo-argentino en las Islas Malvinas como paso previo a una solución final a la disputa de soberanía</p>
+        <p className={styles.titulo}>1976</p>
+          <p>En 1976 se llegó a una situación de tensión bilateral, alimentada por distintos actos unilaterales británicos en el área de la disputa. A fines de ese año, la Asamblea General de las
+             Naciones Unidas, por una amplia mayoría y con la única oposición del Reino Unido, aprobó la Resolución 31/49 que nuevamente reconocía los "continuos esfuerzos" argentinos para 
+             "facilitar el proceso de descolonización y promover el bienestar de la población de las Islas", solicitaba a ambos gobiernos "que aceleren las negociaciones relativas a la disputa sobre 
+             soberanía, según se pide en las resoluciones 2065 (XX) y 3160 (XXVIII)", e instaba a las dos partes a "que se abstengan de adoptar decisiones que entrañen la introducción de modificaciones unilaterales 
+             en la situación mientras las Islas están atravesando por el proceso recomendado en las resoluciones" mencionadas</p>
+        <p className={styles.titulo}>1982</p>
+          <p>Solicita a los dos gobiernos reanudar las negociaciones que permitan encontrar una solución pacífica a la disputa de soberanía. Ello fue reiterado en las subsiguientes 
+            resoluciones de la Asamblea relativas a la Cuestión.</p>
+          <p>Entre 1982 y 1988 la Asamblea General adoptó otras 7 resoluciones sobre la cuestión de las Islas Malvinas en las que reafirmó la existencia de la disputa 
+            de soberanía entre la Argentina y el Reino Unido y llamó ambas partes a reanudar las negociaciones.
+              En ninguna de dichas resoluciones, ni en las adoptadas posteriormente por el Comité Especial de Descolonización,
+               se ha hecho mención al principio de libre determinación. Más aún, en 1985 la Asamblea General rechazó las dos 
+               enmiendas presentadas por el Reino Unido para incluir referencias a dicho principio en el proyecto de resolución.</p>
+        <p className={styles.titulo}>1989</p>
+          <p>Desde 1989 el examen anual de la Cuestión de las Islas Malvinas tiene lugar principalmente en el marco del Comité Especial de Descolonización
+             (C24), compuesto actualmente por 29 Estados Miembro. De ese modo, en junio de cada año el C24 adopta por consenso un proyecto de resolución mediante el que,
+              reafirmando la vigencia de las resoluciones adoptadas por la Asamblea General, reitera que la manera de poner fin a la especial y particular situación colonial en la cuestión 
+              de las Islas Malvinas es la solución pacífica y negociada de la disputa de soberanía entre Argentina y el Reino Unido, solicita a ambos gobiernos que reanuden las negociaciones de soberanía y reitera su apoyo a la misión de buenos oficios del Secretario General.</p>
+      <p className={styles.titulo}>2021</p>
+        <p>El Comité Especial de Descolonización en una resolución adoptada por consenso, reitero qeu la manera de poner fina a la particular situación colonial del archpielago es la solución negociada de la controversia.</p>
+        <p>El texto -promovido por Bolivia, Chile, Cuba, Ecuador, Nicaragua y Venezuela-, lamenta que pese al amplio respaldo internacional a la negociación, todavía no se implementen las numerosas resoluciones de la Asamblea General sobre el tema.</p>
+        <p className={styles.titulo}>Apoyo latinoamericano al reclamo</p>
+        <p>A nombre de la Comunidad de Estados Latinoamericanos y Caribeños  (CELAC), México en voz de su embajador ante la ONU, Ramón De la Fuente, reiteró “el más firme respaldo a los legítimos derechos” de Argentina en la “disputa de la soberanía sobre las Islas Malvinas, Georgias del Sur y Sandwich del Sur y los espacios marítimos circundantes”.</p>
         </div>
       </div>
     </div>
