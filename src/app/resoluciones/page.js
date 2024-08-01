5c1c03d5acc83831746f9b5c42cce088fc369727
Ese Oxalde <ese.oxalde@gmail.com>
2024-08-01 17:38:30 -0300

    ...

--- src/app/resoluciones/page.js
+++ src/app/resoluciones/page.js
@@ -5,13 +5,13 @@ export default function ResolucionesPage() {
     <div className="container my-5 py-5" role="document">
       <h1 className={`text-center subtitle mb-5`} tabIndex="0" >Resoluciones de la ONU</h1>
       <section aria-labelledby="year-1960">
-        <h2 id="year-1960" className={styles.titulo}>1960</h2>
+        <h2 id="year-1960" className="titulo">1960</h2>
           <p>El 14 de diciembre de 1960 la Asamblea General de las Naciones Unidas adoptó la resolución 1514 (XV) “Declaración sobre la concesión de la independencia a los países y pueblos coloniales”.</p>
           <p>Esta resolución marcó un hito en el proceso de descolonización impulsado por las Naciones Unidas, proclamando la necesidad de poner fin, rápida e incondicionalmente, al colonialismo en todas sus formas y manifestaciones,
            y consagrando los dos principios fundamentales que debían guiar el proceso de descolonización: el de libre determinación y el de integridad territorial.</p>
       </section>
       <section aria-labelledby="year-1964">
-        <h2 id="year-1964"className={styles.titulo}>1964</h2>
+        <h2 id="year-1964"className="titulo">1964</h2>
           <p>En septiembre de 1964, durante el debate del Subcomité III del Comité Especial de Descolonización, el Embajador José María Ruda pronunció su célebre alegato donde 
             sostuvo que no existe en las islas una población sojuzgada, subyugada o sometida al colonialismo, siendo ésta una de las razones por las cuales no corresponde 
             a dicha población el derecho a la libre determinación de los pueblos que alega la Parte británica.</p>
@@ -19,22 +19,22 @@ export default function ResolucionesPage() {
               la usurpación y ocupación británica de esa parte del territorio nacional argentino.</p>
        </section>
         <section aria-labelledby="year-1965">
-          <h2 id="year-1965" className={styles.titulo}>1965</h2>
+          <h2 id="year-1965" className="titulo">1965</h2>
           <p>El 16 de diciembre de 1965 la Asamblea General adoptó la resolución 2065 (XX), la primera referida exclusivamente a la Cuestión Malvinas, a través de la cual reconoció la existencia de 
             una disputa de soberanía entre la Argentina y el Reino Unido e invitó a ambos países a negociar para encontrar una solución pacífica a la controversia.</p>
         </section>
         <section aria-labelledby="year-1966-1982">
-        <h2 id="year-1966-1982" className={styles.titulo}>1966-1982</h2>
+        <h2 id="year-1966-1982" className="titulo">1966-1982</h2>
           <p>Se inició un roceso de negociasiones bilaterales entre la Argentina y el Reino Unido que se extendió hasta 1982.
             Durante ese período ambos países analizaron varias hipótesis de solución de la disputa, no pudiendo arribar a un acuerdo.</p>
         </section>
         <section aria-labelledby="year-1973">
-        <h2 id="year-1973" className={styles.titulo}>1973</h2>
+        <h2 id="year-1973" className="titulo">1973</h2>
           <p>La resolución 3160 (XXVIII) de la Asamblea General reconocía los continuos esfuerzos realizados por el gobierno argentino y declaraba la necesidad de que se aceleren las 
           negociaciones previstas en la Resolución 2065 (XX) para arribar a una solución pacífica de la disputa de soberanía existente entre ambos gobiernos sobre las Islas Malvinas.</p>
         </section>
         <section aria-labelledby="year-1976">
-        <h2 id="year-1976" className={styles.titulo}>1976</h2>
+        <h2 id="year-1976" className="titulo">1976</h2>
           <p>En 1976 se llegó a una situación de tensión bilateral, alimentada por distintos actos unilaterales británicos en el área de la disputa. A fines de ese año, la Asamblea General de las
              Naciones Unidas, por una amplia mayoría y con la única oposición del Reino Unido, aprobó la Resolución 31/49 que nuevamente reconocía los continuos esfuerzos argentinos para 
              facilitar el proceso de descolonización y promover el bienestar de la población de las Islas, solicitaba a ambos gobiernos que aceleren las negociaciones relativas a la disputa sobre 
@@ -42,7 +42,7 @@ export default function ResolucionesPage() {
              en la situación mientras las Islas están atravesando por el proceso recomendado en las resoluciones mencionadas</p>
         </section>
         <section aria-labelledby="year-1982">
-        <h2 id="year-1982" className={styles.titulo}>1982</h2>
+        <h2 id="year-1982" className="titulo">1982</h2>
           <p>En noviembre de 1982 la Asamblea General adoptó la resolución 37/9 que solicita a 
             los dos gobiernos reanudar las negociaciones que permitan encontrar una solución pacífica a la disputa de soberanía. Ello fue reiterado en las subsiguientes 
             resoluciones de la Asamblea relativas a la Cuestión.</p>
@@ -53,20 +53,20 @@ export default function ResolucionesPage() {
                enmiendas presentadas por el Reino Unido para incluir referencias a dicho principio en el proyecto de resolución.</p>
         </section>
         <section aria-labelledby="year-1989">
-        <h2 id="year-1989" className={styles.titulo}>1989</h2>
+        <h2 id="year-1989" className="titulo">1989</h2>
           <p>Desde 1989 el examen anual de la Cuestión de las Islas Malvinas tiene lugar principalmente en el marco del Comité Especial de Descolonización
              (C24), compuesto actualmente por 29 Estados Miembro. De ese modo, en junio de cada año el C24 adopta por consenso un proyecto de resolución mediante el que,
               reafirmando la vigencia de las resoluciones adoptadas por la Asamblea General, reitera que la manera de poner fin a la especial y particular situación colonial en la cuestión 
               de las Islas Malvinas es la solución pacífica y negociada de la disputa de soberanía entre Argentina y el Reino Unido, solicita a ambos gobiernos que reanuden las negociaciones de soberanía y reitera su apoyo a la misión de buenos oficios del Secretario General.</p>
         </section>
         <section aria-labelledby="cuarta-comision">
-          <h2 id="cuarta-comision" className={styles.titulo}>Comisión de Política Especial y Descolonización (Cuarta Comisión)</h2>
+          <h2 id="cuarta-comision" className="titulo">Comisión de Política Especial y Descolonización (Cuarta Comisión)</h2>
             <p>la Cuestión Malvinas también es abordada cada año en el debate general sobre temas relativos a la Descolonización que celebra la Comisión de Política Especial y Descolonización (Cuarta Comisión) de la Asamblea General. En dicho marco, la Argentina 
               y otras delegaciones (principalmente de América Latina) realizan intervenciones en capacidad nacional y a nombre de grupos de países como CELAC y MERCOSUR, en las que expresan su apoyo a los derechos de soberanía de nuestro país y reclaman al Reino Unido 
               que ponga fin a la ocupación colonial mediante la reanudación de negociaciones.</p>
         </section>
         <section aria-labelledby="year-2023">
-          <h2 id="year-2023" className={styles.titulo}>2023 - Apoyo internacional</h2>
+          <h2 id="year-2023" className="titulo">2023 - Apoyo internacional</h2>
             <p>El Comité Especial de Descolonización de las Naciones Unidas (C-24) adoptó por consenso una nueva resolución que reitera, como todos los años desde 1983, el llamado al Reino Unido y la Argentina a retomar las negociaciones para encontrar, 
               a la mayor brevedad posible, una solución pacífica y definitiva a la controversia de soberanía sobre las Islas Malvinas, Georgias del Sur, Sandwich del Sur y los espacios marítimos circundantes.</p>
             <p>La resolución adoptada fue copatrocinada por todos los países latinoamericanos integrantes del Comité Especial: Chile, Bolivia, Cuba, Ecuador, Nicaragua y Venezuela. 
