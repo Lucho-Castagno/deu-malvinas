b06ac1cfd5719788ae957df3f3138456d3a5c279
Ese Oxalde <ese.oxalde@gmail.com>
2024-08-01 18:39:20 -0300

    Agregue atributos aria y encabezados no visibles -puede cambiarse si no sirven/gustan

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -22,12 +22,13 @@ export default function Home() {
         </div>
       </section>
       <section className={`container text-center m-5`}>
-        <h1 className={styles.subtitle}>
+        <h1 className={styles.subtitle} tabIndex="0">
           Veteranos que lucharon en Malvinas 
-          <Image src="/soldado.png" alt="Soldado" width={200} height={200} className={styles.subtitleImage} />
+          <Image src="/soldado.png" alt="" width={200} height={200} className={styles.subtitleImage} />
         </h1>
       </section>
-      <section className={`container text-center mb-5`}>
+      <section className={`container text-center mb-5`}  aria-labelledby="photos-heading">
+      <h2 id="photos-heading" className="sr-only">Galería de fotos de soldados argentinos</h2>
         <div className="row">
           <div className="col-lg-4">
             <Image src="/fotos/imagen1.png" alt="Soldados argentinos en una trinchera en Puerto Yapeyú" width={300} height={300} className={styles.photo} />
@@ -40,10 +41,11 @@ export default function Home() {
           </div>
         </div>
       </section>
-      <section className={`container`}>
+      <section className={`container`} aria-labelledby="conflict-description-heading">
+      <h2 id="conflict-description-heading" className="sr-only">Introducción al conflicto</h2>
         <div className="row">
           <div className="col-md-4 text-center">
-            <Image src="/argentina.png" alt="Argentina" width={200} height={200} className={styles.finalImage} />
+            <Image src="/argentina.png" alt="" width={200} height={200} className={styles.finalImage} />
           </div>
           <div className="col-md-8">
             <p>El conflicto armado entre Argentina y el Reino Unido por las Islas Malvinas en 1982 resultó en la trágica pérdida de muchas vidas.
