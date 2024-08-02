f59eba7ad6fb4b2b40022f7a90b9fbe393b50b20
lcastagno <lucho27ris@gmail.com>
2024-08-02 20:02:13 -0300

    fix. PDF resenia

--- src/app/historiaveterano/page.js
+++ src/app/historiaveterano/page.js
@@ -59,27 +59,31 @@ export default function HistoriaVeterano() {
   const baseUrl = "https://www.bienestar.mil.ar/malvinas";
   const defaultPhotoUrl = "/images/soldado2.png"; // Ruta de la imagen por defecto
 
-  function processUrl(url) {
+  function processUrl(url, type) {
     if (url.startsWith(".")) {
       url = url.substring(1);
     }
 
-    const validExtensions = [".png", ".jpg", ".jpeg", ".gif"];
-    const extIndex = url.lastIndexOf(".");
-    if (extIndex !== -1) {
-      const ext = url.substring(extIndex);
-      if (!validExtensions.includes(ext)) {
-        url = url.substring(0, extIndex) + ".png";
+    if (type === "image") {
+      const validExtensions = [".png", ".jpg", ".jpeg", ".gif"];
+      const extIndex = url.lastIndexOf(".");
+      if (extIndex !== -1) {
+        const ext = url.substring(extIndex);
+        if (!validExtensions.includes(ext)) {
+          url = url.substring(0, extIndex) + ".png";
+        }
+      } else {
+        url += ".png";
       }
-    } else {
-      url += ".png";
     }
 
     return url;
   }
 
-  const photoUrl = Foto ? `${baseUrl}${processUrl(Foto)}` : defaultPhotoUrl;
-  const pdfUrl = PDF ? `${baseUrl}${processUrl(PDF)}` : null;
+  const photoUrl = Foto
+    ? `${baseUrl}${processUrl(Foto, "image")}`
+    : defaultPhotoUrl;
+  const pdfUrl = PDF ? `${baseUrl}${processUrl(PDF, "pdf")}` : null;
 
   const renderField = (label, value) => {
     if (!value) return null;
@@ -114,8 +118,8 @@ export default function HistoriaVeterano() {
               </div>
               <div>
                 <h5 className="card-title mb-0">{Nombre}</h5>
-                {renderField(t('rank'), Escalafon)}
-                {renderField(t('document'), DNI)}
+                {renderField(t("rank"), Escalafon)}
+                {renderField(t("document"), DNI)}
                 {pdfUrl && (
                   <a href={pdfUrl} target="_blank" rel="noopener noreferrer">
                     <button
@@ -131,21 +135,21 @@ export default function HistoriaVeterano() {
             <hr />
             <div className="row">
               <div className="col-md-6">
-                {renderField(t('grade'), Grado)}
-                {renderField(t('post_mortem_grade'), Grado_PM)}
-                {renderField(t('weapon'), Arma)}
-                {renderField(t('unit'), Unidad)}
-                {renderField(t('date_of_death'), F_Deceso)}
-                {renderField(t('place_of_death'), L_Deceso)}
-                {renderField(t('mother'), N_Madre)}
-                {renderField(t('father'), N_Padre)}
-                {renderField(t('place_of_burial'), L_Entierro)}
-                {renderField(t('grave'), Tumba)}
-                {renderField(t('date_of_birth'), F_Nac)}
-                {renderField(t('place_of_birth'), L_Nac)}
-                {renderField(t('combat_role'), Rol_Comb)}
-                {renderField(t('date_of_enlistment'), F_Ingreso)}
-                {renderField(t('date_of_promotion'), F_Ascenso)}
+                {renderField(t("grade"), Grado)}
+                {renderField(t("post_mortem_grade"), Grado_PM)}
+                {renderField(t("weapon"), Arma)}
+                {renderField(t("unit"), Unidad)}
+                {renderField(t("date_of_death"), F_Deceso)}
+                {renderField(t("place_of_death"), L_Deceso)}
+                {renderField(t("mother"), N_Madre)}
+                {renderField(t("father"), N_Padre)}
+                {renderField(t("place_of_burial"), L_Entierro)}
+                {renderField(t("grave"), Tumba)}
+                {renderField(t("date_of_birth"), F_Nac)}
+                {renderField(t("place_of_birth"), L_Nac)}
+                {renderField(t("combat_role"), Rol_Comb)}
+                {renderField(t("date_of_enlistment"), F_Ingreso)}
+                {renderField(t("date_of_promotion"), F_Ascenso)}
               </div>
               <div className="col-md-6">
                 <Map Latitud={Latitud} Longitud={Longitud} />
